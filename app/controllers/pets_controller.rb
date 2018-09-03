b23f5fda2c279391a31dc35c96ecba35216bc91e
Jason Viger <jason.viger@gmail.com>
2018-09-03 05:33:21 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,9 +13,13 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner << Owner.create(name: params["owner"]["name"])
+    end
     redirect to "pets/#{@pet.id}"
   end
+   
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
