3ec9119b76d1fc991992767ec05f81282ae3e405
Jason Viger <jason.viger@gmail.com>
2018-09-03 05:58:40 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,10 +31,7 @@ class PetsController < ApplicationController
 
   
   patch '/pets/:id' do 
-       if !params[:pet].keys.include?("owner_id")
-    params[:pet]["owner_id"] = []
-    end
-    #######
+    @pet = Pet.find_by_id(params[:id])
  
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
